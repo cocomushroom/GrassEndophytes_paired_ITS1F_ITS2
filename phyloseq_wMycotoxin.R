945f72756f0ee60209e30a99947c14c380146740
koko <kohsuanchen803@gmail.com>
2019-02-18 22:51:26 -0500

    prune sample without toxin data

--- phyloseq_wMycotoxin.R
+++ phyloseq_wMycotoxin.R
@@ -1,4 +1,5 @@
 ###############################################
+#http://deneflab.github.io/MicrobeMiseq/demos/mothur_2_phyloseq.html
 ########### Starting phyloseq##################
 setwd("/Users/kc178/Documents/Florida_projects/florencia/asilomar_redo/ITS/phylofig")
 library("ape")
@@ -10,6 +11,7 @@ library("ggplot2")
 
 ## Load taxa produced by dada2##
 tax<-readRDS(file = "/Users/kc178/Documents/Florida_projects/florencia/asilomar_redo/ITS/tax_final.rds")
+tax_R<-gsub(".__", "",tax) ## If having problem with tax rank, skip this
 ## Load seqtab.nochim produced by dada2##
 seqtab.nochim<-readRDS(file = "/Users/kc178/Documents/Florida_projects/florencia/asilomar_redo/ITS/seqtabnochim.rds")
 
@@ -45,8 +47,27 @@ row.names(metaM_comQ)<-metaM_comQ$ITS_ID
 
 #merge(metaM, toxin_quality,by="MycoID")
 
-
+## note this still have non fungi
 psM <- phyloseq(otu_table(seqtab.nochim, taxa_are_rows=FALSE), 
                sample_data(metaM_comV), 
-               tax_table(tax))
-psM
+               tax_table(tax_R))
+
+psMfilter <-  prune_samples(sample_data(psM)$MycoID =="No",psM)
+
+psMfilterF <- subset_taxa(psMfilter, Kingdom=="Fungi")
+nreads = sort(sample_sums(psMf)) # if need the lowest read number for a sample to determine the rarification number
+nreads
+
+
+#### Create Rarified table ### 
+set.seed(1111)
+psMfilterF_R = rarefy_even_depth(psMfilterF, sample.size = nreads[[1]])
+saveRDS(psMfilterF_R, "/Users/kc178/Documents/Florida_projects/florencia/asilomar_redo/ITS/psMfilterF_R.rds")
+
+
+#### Metacoder###
+psMfilterF_R<-readRDS(file="/Users/kc178/Documents/Florida_projects/florencia/asilomar_redo/ITS/psMfilterF_R.rds") # already rarified in Phyloseq
+psMfilterF_Rp <- parse_phyloseq(psMfilterF_R)
+print(psMfilterF_Rp) #pay attention to different tables and names
+
+
